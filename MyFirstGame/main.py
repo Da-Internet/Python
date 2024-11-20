16a0a66655f60c1e55e311d3e5201a50bef1b9c5
Da_Internet <diego-canto@hotmail.com>
2024-11-19 21:11:51 -0600

    Codigo de Pelota

--- MyFirstGame/main.py
+++ MyFirstGame/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
 
+from  random import randint
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pygame.init()
 
+#Inicializamos la ventana del Juego
+ventana = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Pin Ball")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Creamos el objeto Pelota
+ball = pygame.image.load("img/Pelota.png")
+ballrect = ball.get_rect()
+speed = [4,4]
+ballrect.move_ip(0,0)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#bucle principal del juego
+jugando = True
+while jugando:
+    #Comprobamos los eventos
+
+    #comprobamos si se ha pulsado el boton de cierre de ventana
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jugando = False
+
+    #muevo la pelota
+    ballrect = ballrect.move(speed)
+
+    #compruebo su la pelota toca el limite de la ventana
+    if ballrect.left < 0 or ballrect.right > ventana.get_width():
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speed[1] = -speed[1]
+    #Se pinta la venta con un color
+
+    #Esto borra los posibles elementos anteriores
+    ventana.fill((255,255,255))
+    ventana.blit(ball,ballrect)
+
+    #Todos los elementos se vuelven a dibujar
+    pygame.display.flip()
+
+    #Controlamos la Frecuencia de Refresco(FPS)
+    pygame.time.Clock().tick(60)
+pygame.quit()
