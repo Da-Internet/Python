6cfdfb0ec54a37865f3b0ac3308737487c75f552
Da_Internet <diego-canto@hotmail.com>
2024-11-26 19:31:19 -0600

    Creacion de brick.py

---
+++ MySecondGame/Models/brick.py
@@ -0,0 +1,10 @@
+
+import pygame
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self,position:tuple):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('Img/wall.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
